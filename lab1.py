b87d99be4d4fe55947fa9f95c2c459967d3ab778
JoeyMartin4 <103144185+JoeyMartin4@users.noreply.github.com>
2023-04-05 14:06:08 -0700

    Lab_1

--- lab1.py
+++ lab1.py
@@ -8,10 +8,13 @@ def main():
     totalCost = 0.0
 
     totalCost = getItemCosts()
+    #print(totalCost)
     taxRate, shipping = getOtherCosts()
+    #print(taxRate, shipping)
     printReceipt(totalCost, taxRate, shipping)
 
     print("Thank you!")
+
 
 # getItemCosts() prompts a user for a list of item
 # costs and returns the sum
@@ -21,12 +24,15 @@ def getItemCosts():
     more = 'y'
 
     while more == 'y':
-        itemcost = float(input("Enter item cost $ "))
+        itemCost = float(input("Enter item cost $ "))
+        #print(itemCost)
         totalCost = totalCost + itemCost
+        #print(totalCost)
         more = input("Do you have more items (y/n): ")
 
     return totalCost
- 
+
+
 # getOtherCosts() prompts a user for the tax rate
 # and shipping costs and returns both inputs
 def getOtherCosts():
@@ -34,18 +40,24 @@ def getOtherCosts():
     shipping = 0.0
 
     taxRate = float(input("\nEnter tax rate (i.e. .075 for 7.5%): "))
+    #print(taxRate)
     shipping = float(input("Enter shipping costs $ "))
+    #print(shipping)
 
-    return shipping, taxRate
+    return taxRate, shipping
 
-# printReceipt() accepts total cost, tax rate, and 
-# shipping costs and calculates and prints the tax 
+
+# printReceipt() accepts total cost, tax rate, and
+# shipping costs and calculates and prints the tax
 # amount, and total cost
 def printReceipt(totalCost, taxRate, shipping):
-    print("\nSubtotal: $ ", format(totalCost, ".2f"))
-    print("Tax: $", format((taxRate), ".2f"))
-    print("Shipping: $", format(shipping,".2f"))
+    taxAmount = totalCost * taxRate
+    #print(taxAmount)
+    print("\nSubtotal: $", format(totalCost, ".2f"))
+    print("Tax: $", format(taxAmount, ".2f"))
+    print("Shipping: $", format(shipping, ".2f"))
     print("------------------------")
-    print("Please pay: $", format(totalCost + taxRate + shipping, ".2f"))
+    print("Please pay: $", format(totalCost + taxAmount + shipping, ".2f"))
+
 
 main()
